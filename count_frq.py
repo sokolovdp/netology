c912ebed62ea2f4a0486dd790155a3e6dc5bd7c8
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-03-03 11:31:19 +0100

    added stemmer and stop words

--- count_frq.py
+++ count_frq.py
@@ -1,11 +1,12 @@
 import json
-import xml
 from xml.dom import minidom
 import os
 import chardet
 import re
 import operator
 from collections import Counter
+import nltk
+from nltk.corpus import stopwords
 
 dir_name = ".\PY1_Lesson_2.3"
 min_length = 3
@@ -54,7 +55,11 @@ def get_all_words(raw_text):
     mytext = clean_text(raw_text)
     return [clean_spets(w.lower()) for w in mytext.split() if len(w) > min_length and not w.isnumeric()]
 
+
 def main():
+    russian_stemmer = nltk.stem.snowball.RussianStemmer(ignore_stopwords=True)
+    my_stop_words = set(stopwords.words('russian')+['такж', 'эт', 'котор', 'год', 'нов'])
+
     for file in os.listdir(dir_name):
         file_name = os.path.join(dir_name, file)
         if file.endswith(".json"):
@@ -71,8 +76,10 @@ def main():
             for news in news_channel:
                 words = get_all_words(str(news['description']))
                 all_json_text += words
-    all_json_words_frq = sorted(Counter(all_json_text).items(), key=operator.itemgetter(1), reverse=True)
-    print (all_json_words_frq[:10])
+
+    clean_json_text = [ w for w in map(russian_stemmer.stem, all_json_text) if w not in my_stop_words ]
+    all_json_words_frq = sorted(Counter(clean_json_text).items(), key=operator.itemgetter(1), reverse=True)
+    print("\n10 наиболее популярных слов в загруженных json текстах:\n", *all_json_words_frq[:10], '\n\n')
 
     all_xml_text = list()
     for file, encod in xml_files:
@@ -83,8 +90,10 @@ def main():
             for item in items_list:
                 words = get_all_words(item.getElementsByTagName('description').item(0).firstChild.nodeValue)
                 all_xml_text += words
-    all_xml_words_frq = sorted(Counter(all_xml_text).items(), key=operator.itemgetter(1), reverse=True)
-    print (all_xml_words_frq[:10])
+
+    clean_xml_text = [w for w in map(russian_stemmer.stem, all_xml_text) if w not in my_stop_words]
+    all_xml_words_frq = sorted(Counter(clean_xml_text).items(), key=operator.itemgetter(1), reverse=True)
+    print ("\n10 наиболее популярных слов в загруженных xml текстах:\n", *all_xml_words_frq[:10], '\n\n')
 
 if __name__ == "__main__":
     main()
